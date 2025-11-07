c933822aed4e911833d7cd170f160f80ffbf4a3b
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-11-08 02:19:45 +0500

    Update plight

--- pkgs/plight/default.nix
+++ pkgs/plight/default.nix
@@ -1,49 +1,20 @@
 {
   lib,
   stdenv,
-  pkg-config,
-  rustPlatform,
-  fetchFromGitHub,
-  dbus,
-  udev,
-  pipewire,
-  llvmPackages,
-  libxcb,
-  xorg,
-}:
-rustPlatform.buildRustPackage {
-  pname = "plight";
-  version = "0.2.0-dev";
-
-  src = fetchFromGitHub {
-    owner = "pguin-sudo";
-    repo = "plight";
-    rev = "addb94bb37eeabb4adf76a7fe434277c1c91aa83";
-    hash = "sha256-GWraAsLEQW5prmbNrA4OypLOfgYO1MpqTae2hTv1Yto=";
-  };
-
-  cargoHash = "sha256-esNxp4MCV50q49MBMCodvyTOpDun36ID1Oqfv+UC9EE=";
-
-  nativeBuildInputs = [pkg-config llvmPackages.clang];
-
-  buildInputs = [
-    pipewire
-    dbus
-    udev
-    llvmPackages.libclang
-    stdenv.cc.libc
-    libxcb
-    xorg.libX11
-  ];
-
-  LIBCLANG_PATH = "${llvmPackages.libclang.lib}/lib";
-  NIX_CFLAGS_COMPILE = "-isystem ${stdenv.cc.libc.dev}/include";
-
-  meta = with lib; {
-    description = "A configurable program for organizing dynamic backlighting";
-    homepage = "https://github.com/pguin-sudo/plight";
-    license = licenses.gpl3Only;
-    maintainers = ["pguin-sudo"];
-    platforms = platforms.linux;
-  };
-}
+  ...
+}: let
+  rev = "2d88511b7cb7f017a338a0c9a79ea7fed699a14c";
+  plightFlake = builtins.getFlake "github:pguin-sudo/plight/${rev}";
+  pkg = plightFlake.packages.${stdenv.hostPlatform.system}.default;
+in
+  pkg.overrideAttrs (oldAttrs: {
+    meta =
+      oldAttrs.meta or {}
+      // (with lib; {
+        description = "A configurable program for organizing dynamic backlighting";
+        homepage = "https://github.com/pguin-sudo/plight";
+        license = licenses.gpl3Only;
+        maintainers = ["pguin-sudo"];
+        platforms = platforms.linux;
+      });
+  })
