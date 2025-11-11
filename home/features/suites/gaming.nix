a412a0a7b139f0e89f75e6285e2d5366a9ac17e0
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-11-11 22:50:30 +0500

    Add discord

---
+++ home/features/suites/gaming.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+with lib; let
+  cfg = config.features.suites.gaming;
+in {
+  options.features.suites.gaming.enable = mkEnableOption "gaming suite";
+
+  config = mkIf cfg.enable {
+    # Steam installed in host config
+
+    home.packages = with pkgs; [
+      discord
+    ];
+  };
+}
