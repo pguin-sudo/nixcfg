c26fa4b68f601ec98d0277e458b6321090b4e230
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-11-17 12:40:36 +0500

    Add libroffice

--- hosts/delta/partitioning.nix
+++ hosts/delta/partitioning.nix
@@ -18,15 +18,41 @@
           ];
         };
       };
-      #      win = {
-      #        type = "disk";
-      #        device = "/dev/disk/by-uuid/a49078ce-3cff-4422-b92e-4b946522bdb2";
-      #        content = {
-      #          type = "filesystem";
-      #format = "ext4";
-      #          mountpoint = "/mnt/win";
-      #        };
-      #      };
+      #win = {
+      #  type = "disk";
+      #  device = "/dev/disk/by-uuid/ata-Samsung_SSD_850_EVO_120GB_S21SNXAGC01969M-part3";
+      #  content = {
+      #    type = "filesystem";
+      #    format = "ntfs3";
+      #    mountpoint = "/mnt/win";
+      #    mountOptions = [
+      #      "rw"
+      #      "uid=1000"
+      #      "gid=100"
+      #      "umask=022"
+      #      "fmask=113"
+      #      "dmask=002"
+      #    ];
+      #  };
+      #};
+
+      #music = {
+      #  type = "disk";
+      #  device = "/dev/disk/by-uuid/wwn-0x50014ee6b006eea5-part2";
+      #  content = {
+      #    type = "filesystem";
+      #    format = "ntfs3";
+      #    mountpoint = "/mnt/music";
+      #    mountOptions = [
+      #      "rw"
+      #      "uid=1000"
+      #      "gid=100"
+      #      "umask=022"
+      #      "fmask=113"
+      #      "dmask=002"
+      #    ];
+      #  };
+      #};
     };
   };
 }
