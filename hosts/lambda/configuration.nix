d8526571ad035d8029581f98cf9da0309ef6eac0
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2025-12-30 04:32:43 +0500

    Enable obs (lambda)

--- hosts/lambda/configuration.nix
+++ hosts/lambda/configuration.nix
@@ -18,6 +18,8 @@
   # AppStores
   common.services.appimage.enable = true;
   common.services.steam.enable = true;
+  # Enable kernel modules for obs-studio
+  common.services.obs-studio.enable = true;
 
   #services.samba.enable = true;
 
