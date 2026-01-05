79d6d7f3c605df9499f4069f527d6974eca95e25
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2026-01-06 02:25:03 +0500

    Add firewall for steam

--- hosts/common/services/steam.nix
+++ hosts/common/services/steam.nix
@@ -1,18 +1,33 @@
-{ config
-, lib
-, ...
+{
+  config,
+  lib,
+  ...
 }:
 with lib; let
   cfg = config.common.services.steam;
-in
-{
+in {
   options.common.services.steam.enable = mkEnableOption "enable steam";
 
   config = mkIf cfg.enable {
     programs.steam = {
       enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+
+    networking.firewall = {
+      enable = true;
+
+      allowedUDPPorts = [24642 27036];
+      allowedTCPPorts = [24643];
+
+      allowedUDPPortRanges = [
+        {
+          from = 27000;
+          to = 27100;
+        }
+      ];
     };
   };
 }
