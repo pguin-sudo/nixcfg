9f2d804a04c98cef430dd8b0c203e642561d83d9
PGuin <138515193+pguin-sudo@users.noreply.github.com>
2026-07-26 12:24:23 +0500

    Add obs-studio

--- hosts/common/services/obs-studio.nix
+++ hosts/common/services/obs-studio.nix
@@ -12,10 +12,12 @@ in {
     boot.extraModulePackages = with config.boot.kernelPackages; [
       v4l2loopback
     ];
-    # Remove because of howdy
-    #boot.kernelModules = ["v4l2loopback"];
+    # Howdy is currently disabled repo-wide, so it no longer conflicts with autoloading here
+    boot.kernelModules = ["v4l2loopback"];
     boot.extraModprobeConfig = ''
-      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+      options v4l2loopback devices=1 video_nr=1 card_label="OBS Virtual Camera" exclusive_caps=1
     '';
+    # So the module can be picked up without sudo/pkexec
+    security.polkit.enable = true;
   };
 }
